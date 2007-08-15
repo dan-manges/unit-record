1341786692104d90132c43ef2e1f4dbd8538d6b1
dcmanges <dcmanges@fc393ee8-1468-4d82-ada8-d2686fec506f>
2007-08-15 03:41:12 +0000

    remove rake task and dump columns on call to disconnect
    git-svn-id: svn+ssh://rubyforge.org/var/svn/unit-test-ar/trunk/unit_record@13 fc393ee8-1468-4d82-ada8-d2686fec506f

--- lib/unit_record/disconnected_active_record.rb
+++ lib/unit_record/disconnected_active_record.rb
@@ -2,11 +2,10 @@ module UnitRecord
   module DisconnectedActiveRecord
     def disconnect!
       columns_file = File.join(RAILS_ROOT, "db", "columns.rb")
-      if File.exists?(columns_file)
-        load columns_file
-      else
-        raise "Could not find columns.rb file to disconnect the database - run rake db:columns:dump"
+      File.open(columns_file, "w") do |file|
+        UnitRecord::ColumnDumper.dump(ActiveRecord::Base.connection, file)
       end
+      load columns_file
       (class << self; self; end).class_eval do
         def connection
           raise "ActiveRecord is disconnected; database access is unavailable in unit tests."
--- tasks/unit_record_tasks.rake
+++ tasks/unit_record_tasks.rake
@@ -1,10 +1,7 @@
 namespace :db do
   namespace :columns do
-    desc "Create a db/columns.rb file that can be used to unit test"
-    task :dump => :environment do
-      File.open("db/columns.rb", "w") do |file|
-        UnitRecord::ColumnDumper.dump(ActiveRecord::Base.connection, file)
-      end
+    task :dump do
+      raise "The db:columns:dump rake task is no longer necessary for disconnecting tests from the database"
     end
   end
 end
