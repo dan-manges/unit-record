9682e4113f40b02fd254e747522c45585b326455
dcmanges <dcmanges@fc393ee8-1468-4d82-ada8-d2686fec506f>
2008-02-09 05:03:33 +0000

    give exception message if trying to use fixtures instead of failing silently
    git-svn-id: svn+ssh://rubyforge.org/var/svn/unit-test-ar/trunk/unit_record@45 fc393ee8-1468-4d82-ada8-d2686fec506f

--- lib/unit_record/disconnected_test_case.rb
+++ lib/unit_record/disconnected_test_case.rb
@@ -2,6 +2,12 @@ module UnitRecord
   module DisconnectedTestCase
     def disconnect!
       self.use_transactional_fixtures = false
+      
+      class_eval do
+        def self.fixtures(*args)
+          raise "Fixtures cannot be used with UnitRecord. ActiveRecord is disconnected; database access is unavailable in unit tests."
+        end
+      end
     end
   end
 end
--- test/functional/disconnected_test_case_test.rb
+++ test/functional/disconnected_test_case_test.rb
@@ -4,4 +4,16 @@ functional_tests do
   test "use_transactional_fixtures is false" do
     assert_equal false, Test::Unit::TestCase.use_transactional_fixtures
   end
+  
+  test "trying to use fixtures gives useful message" do
+    exception = nil
+    begin
+      Class.new(Test::Unit::TestCase) do
+        fixtures :users
+      end
+    rescue => exception
+    end
+    assert_not_nil exception
+    assert_equal "Fixtures cannot be used with UnitRecord. ActiveRecord is disconnected; database access is unavailable in unit tests.", exception.message
+  end
 end
